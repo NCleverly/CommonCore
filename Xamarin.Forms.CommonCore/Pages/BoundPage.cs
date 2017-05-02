da4340b3671378241cfe09cc62d3109f9b058bb5
Les Brown <Les.Brown@valorepartners.com>
2017-05-02 13:14:08 -0700

    SetAutomationIds committed

--- Xamarin.Forms.CommonCore/Pages/BoundPage.cs
+++ Xamarin.Forms.CommonCore/Pages/BoundPage.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection;
 using Xamarin.Forms;
 
 namespace Xamarin.Forms.CommonCore
@@ -19,6 +20,36 @@ namespace Xamarin.Forms.CommonCore
 			if (Navigation != null)
 				AppData.AppNav = Navigation;
 			base.OnAppearing();
+		}
+
+		/// <summary>
+		/// Sets the automation identifiers for all class fields and properties of the type View
+		/// </summary>
+		protected void SetAutomationIds()
+		{
+			var bindingFlags = BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public;
+			var fields = this.GetType().GetFields(bindingFlags);
+			foreach (var field in fields)
+			{
+				var fObj = field.GetValue(this);
+				if (fObj is View)
+				{
+					var ctrl = (View)fObj;
+					if (string.IsNullOrEmpty(ctrl.AutomationId))
+						ctrl.AutomationId = field.Name;
+				}
+			}
+			var props = this.GetType().GetProperties(bindingFlags);
+			foreach (var prop in props)
+			{
+				var pObj = prop.GetValue(this);
+				if (pObj is View)
+				{
+					var ctrl = (View)pObj;
+					if (string.IsNullOrEmpty(ctrl.AutomationId))
+						ctrl.AutomationId = prop.Name;
+				}
+			}
 		}
 
 	}
