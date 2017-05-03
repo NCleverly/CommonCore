e53f49959c8304dd57c4e4f1e80967911e0c4fdf
Les Brown <Les.Brown@valorepartners.com>
2017-05-03 11:36:18 -0700

    updated effects pages

--- referenceguide/referenceguide/Views/EffectsPages/EffectsMain.cs
+++ referenceguide/referenceguide/Views/EffectsPages/EffectsMain.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Xamarin.Forms;
+using Xamarin.Forms.CommonCore;
 
 namespace referenceguide
 {
@@ -8,6 +9,51 @@ namespace referenceguide
 		public EffectsMain()
 		{
 			this.Title = "Effects";
+
+			var removeEmptyRowsEffect = new GradientButton()
+            {
+                Text = "Remove Empty Cell",
+                Style = AppStyles.LightOrange,
+                AutomationId = "removeEmptyRowsEffect",
+                Command = new Command(async (obj) =>
+                {
+                    await Navigation.PushAsync(new ListViewEffect());
+                })
+            };
+
+			var disableWebViewScrollEffect = new GradientButton()
+			{
+				Text = "WebView Scroll Disable",
+				Style = AppStyles.LightOrange,
+				AutomationId = "disableWebViewScrollEffect",
+				Command = new Command(async (obj) =>
+				{
+					await Navigation.PushAsync(new WebViewEffect());
+				})
+			};
+
+			var hideListSeparatorEffect = new GradientButton()
+			{
+				Text = "Hide List Separator",
+				Style = AppStyles.LightOrange,
+				AutomationId = "hideListSeparatorEffect",
+				Command = new Command(async (obj) =>
+				{
+					await Navigation.PushAsync(new ListViewEffect(true));
+				})
+			};
+
+			var stack = new StackLayout()
+			{
+				Padding = 20,
+				Spacing = 10,
+				Children = { removeEmptyRowsEffect, disableWebViewScrollEffect, hideListSeparatorEffect }
+			};
+
+			Content = new ScrollView()
+			{
+				Content = stack
+			};
 		}
 	}
 }
